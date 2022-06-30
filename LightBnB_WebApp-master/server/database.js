3b3340d84f9b6b777e7502736e708b2b8e682f1e
Dylan Wang <wyc6990906@hotmail.com>
2022-06-30 15:50:14 -0600

    add property

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -121,7 +121,7 @@ const getAllProperties = function (options, limit = 10) {
   let queryString = `
       SELECT properties.*, avg(property_reviews.rating) as average_rating
       FROM properties
-      JOIN property_reviews ON properties.id = property_id
+               JOIN property_reviews ON properties.id = property_id
       WHERE TRUE
   `;
   if (options.city) {
@@ -176,9 +176,33 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  if (property.description === undefined) property.description = '';
+  const values = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+    property.country,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code
+  ];
+  const queryString = `
+      INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night,
+                              parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province,
+                              post_code)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;
+  `;
+  return client.query(queryString, values)
+    .then(res => {
+      if (res.rows) return res.rows[0];
+      else return null;
+    });
 }
 exports.addProperty = addProperty;
